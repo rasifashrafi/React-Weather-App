3a89a624830c4ac983bffb926a7a03ced251709f
rasif <rashrafi@techlaunch.com>
2019-05-07 10:00:37 -0400

    first commit

---
+++ precache-manifest.58474f6c4c22536230953b5e2783ff69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73649db39fa5d86660bf",
+    "url": "/React-Weather-App/static/js/runtime~main.73649db3.js"
+  },
+  {
+    "revision": "faf982fc398cf41a8fcd",
+    "url": "/React-Weather-App/static/js/main.faf982fc.chunk.js"
+  },
+  {
+    "revision": "95f9d769ecae48139c17",
+    "url": "/React-Weather-App/static/js/2.95f9d769.chunk.js"
+  },
+  {
+    "revision": "faf982fc398cf41a8fcd",
+    "url": "/React-Weather-App/static/css/main.59e13241.chunk.css"
+  },
+  {
+    "revision": "e6c4fe34483134497dedbcf1e49ba069",
+    "url": "/React-Weather-App/index.html"
+  }
+];
